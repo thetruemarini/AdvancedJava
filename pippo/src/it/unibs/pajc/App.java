93abe8f250c1c8a45444b43513eaf6c9d6f496f1
thetrue <riccardomarini003@gmail.com>
2024-11-11 11:31:25 +0100

    prova3

--- pippo/src/it/unibs/pajc/App.java
+++
@@ -1,8 +0,0 @@
-package it.unibs.pajc;
-public class App {
-    public static void main(String[] args) throws Exception {
-        
-        Colorizer.ANSI_BLUE.print("questo testo sarà blu\n");
-        Colorizer.ANSI_RED.print("questo testo sarà rosso\n");
-    }
-}
