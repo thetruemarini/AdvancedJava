18f095c91e5b5dcc4f1732e9f794099eec490ace
thetrue <riccardomarini003@gmail.com>
2024-11-11 17:29:12 +0100

    colorized wa

--- WhatsApp/src/it/unibs/pajc/WAServer.java
+++ WhatsApp/src/it/unibs/pajc/WAServer.java
@@ -3,9 +3,12 @@ package it.unibs.pajc;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import it.unibs.pajc.color.Colorizer;
+
 public class WAServer {
+    public static final Colorizer ANSI_RED = new Colorizer("\033[0;31m"); // RED
     public static void main(String[] srgs){
-        System.out.println("Avvio del server...");
+        System.out.println(ANSI_RED + "Avvio del server...");
 
         try(
             ServerSocket server = new ServerSocket(1234);
---
+++ WhatsApp/src/it/unibs/pajc/color/Colorizer.java
@@ -0,0 +1,33 @@
+package it.unibs.pajc.color;
+
+public class Colorizer {
+
+    public static final Colorizer ANSI_RESET = new Colorizer("\u001B[0m"); // RESET
+    public static final Colorizer ANSI_BLACK = new Colorizer("\033[0;30m"); // BLACK
+    public static final Colorizer ANSI_RED = new Colorizer("\033[0;31m"); // RED
+    public static final Colorizer ANSI_GREEN = new Colorizer("\033[0;32m"); // GREEN
+    public static final Colorizer ANSI_YELLOW = new Colorizer("\033[0;33m"); // YELLOW
+    public static final Colorizer ANSI_BLUE = new Colorizer("\033[0;34m"); // BLUE
+    public static final Colorizer ANSI_PURPLE = new Colorizer("\033[0;35m"); // PURPLE
+    public static final Colorizer ANSI_CYAN = new Colorizer("\033[0;36m"); // CYAN
+    public static final Colorizer ANSI_WHITE = new Colorizer("\033[0;37m"); // WHITE
+
+    private final String ansi_code;
+
+    public Colorizer(String ansi_code) {
+        this.ansi_code = ansi_code;
+    }
+
+    public String colorize(String string) {
+        return ansi_code + string + ANSI_RESET;
+    }
+
+    public void print(String s) {
+        System.out.print(colorize(s));
+    }
+
+    public String toString() {
+        return ansi_code;
+    }
+
+}
